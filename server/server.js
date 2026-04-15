81287142514a4df208463456affafbda846c26c4
User <iceberg238@gmail.com>
2026-04-15 21:51:18 +0500

    JWT

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
+const marketRoutes = require("./routes/market"); // 👈 ДОБАВИЛ
 
 const app = express();
 
@@ -11,5 +12,10 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/profile", profileRoutes);
+app.use("/market", marketRoutes); // 👈 ВАЖНО
+
+app.get("/", (req, res) => {
+  res.json({ ok: true, message: "Server running 🚀" });
+});
 
 app.listen(5000, () => console.log("Server running"));
