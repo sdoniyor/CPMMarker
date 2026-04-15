04970ce96de6120628ab81d85fafc0a367707463
User <iceberg238@gmail.com>
2026-04-15 21:49:13 +0500

    JWT

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
+import MarketPage from "./pages/MarketPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<AuthPage />} />
+
+        <Route path="/market" element={<MarketPage />} />
+
         <Route path="/profile" element={<ProfilePage />} />
+
       </Routes>
     </BrowserRouter>
   );
