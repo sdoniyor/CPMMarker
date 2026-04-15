0e7c1adbe2cc8fd02d3988b9ac580b4af2001478
User <iceberg238@gmail.com>
2026-04-15 22:12:06 +0500

    JWT

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
 import MarketPage from "./pages/MarketPage";
+
+/* Layout wrapper */
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
 
+        {/* AUTH без navbar */}
         <Route path="/" element={<AuthPage />} />
 
-        <Route path="/market" element={<MarketPage />} />
+        {/* MARKET с navbar */}
+        <Route
+          path="/market"
+          element={
+            <Layout>
+              <MarketPage />
+            </Layout>
+          }
+        />
 
-        <Route path="/profile" element={<ProfilePage />} />
+        {/* PROFILE с navbar */}
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <ProfilePage />
+            </Layout>
+          }
+        />
 
       </Routes>
     </BrowserRouter>
