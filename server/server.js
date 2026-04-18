3c8424dbda869a97c2654f2da1ca62d619b5f085
User <iceberg238@gmail.com>
2026-04-18 11:51:41 +0500

    fix/

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.use(
 );
 
 /* 🔥 IMPORTANT: preflight support */
-app.options("*", cors());
+app.options("/*", cors());
 
 /* ================= BODY ================= */
 app.use(express.json({ limit: "10mb" }));
